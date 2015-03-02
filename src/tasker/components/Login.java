0490ff948baf701c9a182f16fb253f206955b143
Jesse Dahir-Kanehl <daisysgreenhouse@gmail.com>
2015-03-02 01:52:29 -0600

    adding the layout for basic tasker

--- src/tasker/components/Login.java
+++ src/tasker/components/Login.java
@@ -1,17 +1,12 @@
 package tasker.components;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.PrintWriter;
-
-import survey.components.PrintWriter;
 import tasker.dao.TaskerDao;
 import tasker.model.User;
 
@@ -48,23 +43,12 @@ public class Login extends HttpServlet {
 		.password("Evil")
 		.build();
 	
-	String header = "<head>\n" + 
-			"    <meta http-equiv=\"Content-Type\" content=\"text/html\">\n" + 
-			"    <title>Survey</title>\n" + 
-			"    <link rel=\"stylesheet\" href=\"tasker.css\" type=\"text/css\" /> \n" + 
-			"    <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css\">\n" + 
-			"\n" + 
-			"    <script src=\"https://code.jquery.com/jquery-2.1.3.min.js\"></script>\n" + 
-			"    <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js\"></script>\n" + 
-			"    <script src=\"tasker.js\"></script>\n" + 
-			"</head>";
-	
 	public static boolean validated (HttpServletRequest request) {
 		Cookie[] cookies = request.getCookies();
 		for (int i = 0; i < cookies.length; i++) {
 			if (cookies[i].getName().equals("sid")) {
 				String sid = cookies[i].getValue();
-				return data.authenticateUser(sid);
+				return database.authenticateUser(sid);
 			}
 		}
 		return false;
@@ -74,7 +58,7 @@ public class Login extends HttpServlet {
 	@Override
 	public void doGet (HttpServletRequest request, HttpServletResponse response) 
 					throws ServletException, java.io.IOException {
-		response.sendRedirect("/login.html");
+		response.sendRedirect("/tasker/login.html");
 	}
 	
 	@Override
@@ -91,23 +75,11 @@ public class Login extends HttpServlet {
 		if (sid != "") {
 			Cookie validate = new Cookie ("sid", sid);
 			response.addCookie(validate);
+			response.sendRedirect("/tasker/tasks");
 			
-			response.setContentType("text/html;charset=UTF-8");
-			PrintWriter writer = response.getWriter();
-			writer.println("<!DOCTYPE html>");
-			writer.println(header);
-			writer.print("<body><div class=\"container\">");
-			writer.print("<div class=\"row\"> <div class=\"col-lg-2\">");
-			writer.print("<h1>Tasker</h1> </div> </div>");
-			writer.print("<div class=\"row\"> <div class=\"col-md-9\">");
-			writer.print("<form class=\"form-inline\" method=\"post\" action=\"/tasker/Tasker\">");
-			writer.print("<input name=\"description\" type=\"text\" placeholder=\"task\" class=\"form-control\">");
-			writer.print("<input name=\"color\" type=\"color\" placeholder=\"black\" class=\"form-control\">");
-			writer.print("<input name=\"date\" type=\"date\" placeholder=\"mm/dd/yyyy\" class=\"form-control\">");
-			writer.print("<input name=\"submit\" type=\"submit\" value=\"add\" class=\"form-control\">");
 		}
 		else
-			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			response.sendRedirect("/tasker/login?error=1.html");
 		
 		
 		
--- src/tasker/components/Tasker.java
+++ src/tasker/components/Tasker.java
@@ -1,10 +1,10 @@
 package tasker.components;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,14 +29,39 @@ public class Tasker extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		if (Login.validated(request)) {
-			
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = response.getWriter();
+			writer.println("<!DOCTYPE html>");
+			writer.println(header);
+			writer.print("<body><div class=\"container\">");
+			writer.print("<div class=\"row\"> <div class=\"col-lg-2\">");
+			writer.print("<h1>Tasker</h1> </div> </div>");
+			writer.print("<div class=\"row\"> <div class=\"col-md-9\">");
+			writer.print("<form class=\"form-inline\" method=\"post\" action=\"/tasker/Tasker\">");
+			writer.print("<input name=\"description\" type=\"text\" placeholder=\"task\" class=\"form-control\">");
+			writer.print("<input name=\"color\" type=\"color\" placeholder=\"black\" class=\"form-control\">");
+			writer.print("<input name=\"date\" type=\"date\" placeholder=\"mm/dd/yyyy\" class=\"form-control\">");
+			writer.print("<input name=\"submit\" type=\"submit\" value=\"add\" class=\"form-control\"> ");
+			writer.print("</form> </div> </div>");
+			writer.print("<div class=\"row\"> <div class=\"col-md-4\">");
+			writer.print("<form method=\"get\" action=\"/tasker/tasks\">");
+			writer.print("<input name=\"show\" type=\"submit\" value=\"Show only incomplete\" class=\"form-control\">");
+			writer.print("</form> </div> </div>");
+			writer.print("<div class=\"row\"> <div class=\"col-md-9\">");
+			writer.print("<form method=\"get\" action=\"/tasker/tasks\">");
+			writer.print("<table class=\"table\"> <tr> <th>Description</th> <th>Color</th> <th>Due</th> <th>Completed</th> <th></th>");
+			writer.print("</table> </form> </div> </div> </div> </body> </html>");
 		}
+		else
+			response.sendRedirect("/tasker/login.html");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		if (Login.validated(request)) {
 			
 		}
+		else
+			response.sendRedirect("/login.html");
 	}
 	
 	
